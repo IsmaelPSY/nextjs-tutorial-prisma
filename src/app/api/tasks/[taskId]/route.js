77d7e4cb6a11e4204c7f3ec712ddeb85f4990858
IsmaelPSY <ismaelpsy.12@gmail.com>
2023-10-16 21:50:58 -0500

    set api routes and methods

--- src/app/api/tasks/[taskId]/route.js
+++ src/app/api/tasks/[taskId]/route.js
@@ -1,6 +1,36 @@
+import { prisma } from "@/libs/prisma";
 import { NextResponse } from "next/server";
 
-export function GET(reques, { params }) {
-  console.log({params})
-  return NextResponse.json({ message: "ok" + params.taskId })
+export async function GET(request, { params }) {
+  const task = await prisma.task.findUnique({
+    where: {
+      id: Number(params.taskId)
+    }
+  })
+  return NextResponse.json(task)
+}
+
+export async function PUT(request, { params }) {
+  const data = await request.json()
+
+  const task = await prisma.task.update({
+    where: {
+      id: Number(params.taskId)
+    },
+    data: data
+  })
+  return NextResponse.json(task)
+}
+
+export async function DELETE(request, { params }) {
+  try {
+    const task = await prisma.task.delete({
+      where: {
+        id: Number(params.taskId)
+      }
+    })
+    return NextResponse.json(task)
+  } catch (error) {
+    return NextResponse.json(error.message)
+  }
 }
